cefbabf1ab190726be44b384fd928dda1ab40107
anester <bignester@gmail.com>
2013-11-05 22:33:47 -0600

    Reference Issue:#5 and #4 Fixed up Create User page a bit.

--- WorkoutWebApp/Scripts/View/Account/Create.js
+++ WorkoutWebApp/Scripts/View/Account/Create.js
@@ -1,15 +1,44 @@
 ﻿$(function () {
+    var $msgemail = $('#msgemail'),
+        $msghandle = $('#msghandle');
+
     $('#tbemail').change(function () {
         var $this = $(this),
             $mssgbox = $('#msgemail'),
-            val = $this.val();
+            val = $this.val(),
+            $btn = $('#btncreate');
 
-        $.post('/api/AccountChecks/email', { FieldName: 'email', FieldValue: val }, function (data) {
+        $.post('/api/AccountChecks', { FieldName: 'email', FieldValue: val }, function (data) {
             if (data) {
                 $mssgbox.show();
+                $btn.prop('disabled', true);
             }
             else {
                 $mssgbox.hide();
+                if (!$msgemail.is(':visible') && !$msghandle.is(':visible')) {
+                    $btn.prop('disabled', false);
+                }
+            }
+        }).error(function (e) {
+        });
+    });
+
+    $('#tbhandle').change(function () {
+        var $this = $(this),
+            $mssgbox = $('#msghandle'),
+            val = $this.val(),
+            $btn = $('#btncreate');
+
+        $.post('/api/AccountChecks', { FieldName: 'handle', FieldValue: val }, function (data) {
+            if (data) {
+                $mssgbox.show();
+                $btn.prop('disabled', true);
+            }
+            else {
+                $mssgbox.hide();
+                if (!$msgemail.is(':visible') && !$msghandle.is(':visible')) {
+                    $btn.prop('disabled', false);
+                }
             }
         }).error(function (e) {
         });
